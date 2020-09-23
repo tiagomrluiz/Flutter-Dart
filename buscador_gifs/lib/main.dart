6ec8949534ac60779a04ab9a6753d2c1cce3d329
Tiago Morais Rosa Luiz <tiagomrluiz@gmail.com>
2020-09-22 21:23:20 -0300

    commit new project gifs

---
+++ buscador_gifs/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:buscador_gifs/ui/home_page.dart';
+import 'package:buscador_gifs/ui/gif_page.dart';
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(
+        hintColor: Colors.white,
+    inputDecorationTheme: InputDecorationTheme(border:
+    OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+    enabledBorder:
+    OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+    focusedBorder:
+    OutlineInputBorder(borderSide: BorderSide(color: Colors.white))
+    )),
+  ));
+
+}
+
+
